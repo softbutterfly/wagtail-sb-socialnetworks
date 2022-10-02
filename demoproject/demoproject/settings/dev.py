a99bc1c9e4a3ab1ff59d357c427c4f90dee6e3aa
zodiacfirework <martin.vuelta@gmail.com>
2022-10-02 09:33:59 -0500

    Add demoproject

---
+++ demoproject/demoproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-#w=6y15s4m$q=s71i+bzxidxv2@k^538#7fszwq-b+nv^b5%ps"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ demoproject/home/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from wagtail.models import Page
+
+
+class HomePage(Page):
+    pass
